6a413f25347dafd34814f067b867f783170d6e17
= <=>
2024-10-08 17:15:46 +0700

    Python

---
+++ 10 Rumus Geometri/Limas Segitiga.py
@@ -0,0 +1,8 @@
+# Dibuat Oleh : Bama Maulana Wibisana
+# Tanggal Pengerjaan : 08 Oktober 2024
+# Program Menghitung Rumus Limas Segitiga
+
+print("="*20)
+print("Rumus Limas Segitiga")
+print("="*20)
+
