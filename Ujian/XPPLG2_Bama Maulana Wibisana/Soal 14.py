09df0661185c7aed4a3eac56af54ae4164f01b93
= <=>
2024-10-10 13:11:38 +0700

    Python

--- Ujian/XPPLG2_Bama Maulana Wibisana/Soal 14.py
+++
@@ -1,8 +0,0 @@
-huruf = input("Masukkan sebuah huruf (A-Z): ").upper()
-
-if huruf in 'AIUEO':
-    print(huruf, "adalah vokal")
-elif huruf in 'BCDFGHJKLMNPQRSTVWXYZ':
-    print(huruf, "adalah konsonan")
-else:
-    print(huruf, "bukan huruf alfabet")
--- Ujian/XPPLG2_Bama Maulana Wibisana/Soal 3.py
+++
@@ -1,4 +0,0 @@
-QTY = 23
-harga_sepatu = 45000
-
-print("Hasil perkalian qty dan harga sepatu:", QTY * harga_sepatu)
