0c0ad40a01121775f0387020ab240202c6a570c0
= <=>
2024-10-14 10:06:25 +0700

    Python

--- Geometri/10. Limas Segitiga.py
+++ Geometri/10. Limas Segitiga.py
@@ -6,3 +6,14 @@ print("="*20)
 print("Rumus Limas Segitiga")
 print("="*20)
 
+a = int(input("Alas\t: "))
+s1 = int(input("Sisi 1\t: "))
+s2 = int(input("Sisi 2\t: "))
+s3 = int(input("Sisi 3\t: "))
+t = int(input("Tinggi\t: "))
+
+l = 1/2 * a * (s1 + s2 + s3) + 1/2 * a
+v = 1/3 * a * t
+
+print(f"Luas Permukaan\t: {l}")
+print(f"Volume\t: {v}")
